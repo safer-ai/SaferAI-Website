617ec87d3a4c17e1b7508188aff3a53e307ed8a5
Fabien Roger <efab.roger@gmail.com>
2022-09-19 13:28:14 +0200

    Add ping + fix requirements

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, send_file
 import os
+import requests
 
 app = Flask(__name__, static_url_path="", static_folder="frontend/build")
 
@@ -8,6 +9,19 @@ app = Flask(__name__, static_url_path="", static_folder="frontend/build")
 def fun():
     print("hey", __file__[:-6] + "frontend/build/index.html")
     return send_file(__file__[:-6] + "frontend/build/index.html")
+
+
+@app.route("/ping")
+def fun():
+    url = "http://www.kite.com"
+    timeout = 5
+    try:
+        _ = requests.get(url, timeout=timeout)
+        print("Connected to the Internet")
+        return "Connected to the Internet"
+    except (requests.ConnectionError, requests.Timeout) as exception:
+        print("No internet connection.")
+        return "No"
 
 
 if __name__ == "__main__":
