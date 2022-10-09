9094676dfc013f1ff13a30e92ce69aa8bb73052d
Fabien Roger <efab.roger@gmail.com>
2022-10-09 20:08:47 +0200

    Improve conclusion + add footer

--- frontend/src/CountergenResults.tsx
+++ frontend/src/CountergenResults.tsx
@@ -11,6 +11,7 @@ import {
   CardHeader,
 } from "@mui/material";
 import { doublebindExplanation, stereotypesExplanation } from "./params";
+import Footer from "./coutergenmain_sections/Footer";
 
 const Image = (props: { name: string }) => {
   const { name } = props;
@@ -126,197 +127,233 @@ const experimentData = [
 
 const CountergenResults = () => {
   return (
-    <Container maxWidth="lg">
-      <h1>Results On Gender Bias</h1>
-      <p>
-        <i>
-          <b>Results are preliminary</b>: larger real life experiments are
-          needed to assess the viability of this approach. Please view these
-          results as hints aimed at helping you if you want to experiment with
-          model editing.
-        </i>
-      </p>
-      <p>
-        <i>More results will come in the next months!</i>
-      </p>
-      <Card className="section">
-        <CardHeader
-          className="section-title"
-          title=" Observation 0: In some situation, language models exhibit strong
+    <>
+      <Container maxWidth="lg">
+        <h1>Results On Gender Bias</h1>
+        <p>
+          <i>
+            <b>Results are preliminary</b>: larger real life experiments are
+            needed to assess the viability of this approach. Please view these
+            results as hints aimed at helping you if you want to experiment with
+            model editing.
+          </i>
+        </p>
+        <p>
+          <i>More results will come in the next months!</i>
+        </p>
+        <Card className="section">
+          <CardHeader
+            className="section-title"
+            title=" Observation 0: In some situation, language models exhibit strong
           behavior changes based on the gender of the subjects"
-        />
-        <CardContent className="section-content">
-          <p>
-            We compare the probability that a model generate one of the expected
-            outputs when the subjects of the input are female and when they are
-            male. The given number is the relative probability (compared to the
-            biggest of the two probabilities).
-          </p>
-          <p>Data:</p>
-          <ul>
-            <li>{doublebindExplanation}</li>
-            <li>{stereotypesExplanation}</li>
-          </ul>
-          <Table sx={{ minWidth: 650 }} aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell>Model used</TableCell>
-                <TableCell>Female stereotype</TableCell>
-                <TableCell>Male stereotype</TableCell>
-                <TableCell>
-                  Positive prosocial adjective in the double bind experiment
-                </TableCell>
-                <TableCell>
-                  Negative antisocial adjective in the double bind experiment
-                </TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {experimentData.map((data) => (
-                <SimpleTableRow data={data} />
-              ))}
-            </TableBody>
-          </Table>
-          <p>
-            <small>
-              <i>*0 not included in the p=0.05 confidence interval</i>
-            </small>
-          </p>
-        </CardContent>
-      </Card>
+          />
+          <CardContent className="section-content">
+            <p>
+              We compare the probability that a model generate one of the
+              expected outputs when the subjects of the input are female and
+              when they are male. The given number is the relative probability
+              (compared to the biggest of the two probabilities).
+            </p>
+            <p>Data:</p>
+            <ul>
+              <li>{doublebindExplanation}</li>
+              <li>{stereotypesExplanation}</li>
+            </ul>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Model used</TableCell>
+                  <TableCell>Female stereotype</TableCell>
+                  <TableCell>Male stereotype</TableCell>
+                  <TableCell>
+                    Positive prosocial adjective in the double bind experiment
+                  </TableCell>
+                  <TableCell>
+                    Negative antisocial adjective in the double bind experiment
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {experimentData.map((data) => (
+                  <SimpleTableRow data={data} />
+                ))}
+              </TableBody>
+            </Table>
+            <p>
+              <small>
+                <i>*0 not included in the p=0.05 confidence interval</i>
+              </small>
+            </p>
+          </CardContent>
+        </Card>
 
-      <h2>Editing Method</h2>
-      <ul>
-        <li>
-          We measured model activations on the stereotype dataset and edited the
-          model to reduce this bias by applying INLP or RLACE on intermediate
-          activations of the neural network.
-        </li>
-        <li>
-          Finding the relevant directions in the activations is a noisy process.
-          We repeat the process 5 times and show the result of each run.
-        </li>
-        <li>
-          We then measured the relative probability between completions
-          following inputs with a female subject and inputs with male subjects
-          on both stereotypes data and the double bind data as validation.
-        </li>
-      </ul>
-      <Card className="section">
-        <CardHeader
-          className="section-title"
-          title="Observation 1: Bias is easier to remove in the middle of the network"
-        />
-        <CardContent className="section-content">
-          <p>
-            Applying RLACE or INLP to intermediate activations slightly reduces
-            the bias of the model. But it works better and generalize a bit more
-            to the validation set (the double bind sociology experiment) when
-            editing is done in the middle of the network, where the network
-            probably has a crisper representation of "male" and "female".
-          </p>
-          <Grid container>
-            <Grid item xs={12} md={6}>
-              <Image name="inlp_8d_layer" />
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <Image name="rlace_8d_layer" />
-            </Grid>
-          </Grid>
-        </CardContent>
-      </Card>
-      <Card className="section">
-        <CardHeader
-          className="section-title"
-          title="Observation 2: The bias is not encoded in a linear way"
-        />
-        <CardContent className="section-content">
-          <p>
-            Though RLACE is able to make any linear classifier fail separation
-            between male and female with a rank-8 projection, we found that, no
-            matter where the projection is done, and how many dimensions are
-            removed, model bias isn't eliminated.
-          </p>
-          <Grid container>
-            <Grid item xs={12} md={6}>
-              <Image name="rlace_8d_layer" />
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <Image name="rlace_dims" />
-            </Grid>
-          </Grid>
-        </CardContent>
-      </Card>
-
-      <h2>Conclusion</h2>
-      <p>
-        Model exhibit easily measurable bias on small datasets from the
-        literature augmented using CounterGen. Editing model on these small
-        datasets is hard, and further research is need to determine if it can be
-        a robust way to reduce the amount of bias in language models.
-      </p>
-      <p>
-        We ran experiments in other setups, editing multiple layers at once,
-        editing more layers, and trying methods to find the relevant directions
-        faster. If you want to know more, please reach out!
-      </p>
-      <h2>Additional remarks</h2>
-      <h3>Comparison with picking random dimensions</h3>
-      <p>
-        Projecting activation on random directions increases noise, and reduces
-        bias a little when many dimensions are removed. It performs worse than
-        applying INLP and RLACE to intermediate activations.
-      </p>
-      <Grid container>
-        <Grid item xs={12} md={6}>
-          <Image name="rdm_dims" />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <Image name="rdm_8d_layer" />
-        </Grid>
-      </Grid>
-      <h3>Why are performance before edit different?</h3>
-      <p>
-        For each experiment, we pick the variations once. Because no new
-        variation is generated when editing the model, this is a fair and
-        slightly less noisy reference point we can compare the editing methods
-        against.
-      </p>
-      <h3>Where can I find the detail of the experiments?</h3>
-      <p>Here are the notebooks used to produce the results above:</p>
-      <ul>
-        <li>
-          <a href="https://github.com/FabienRoger/Countergen/blob/main/countergen/exploration/gpt_experiment.ipynbr">
-            https://github.com/FabienRoger/Countergen/blob/main/countergen/exploration/gpt_experiment.ipynbr
-          </a>{" "}
-          (Experiment on OpenAI's models)
-        </li>
-        <li>
-          <a href="https://www.kaggle.com/code/fabienroger/editing-2r">
-            https://www.kaggle.com/code/fabienroger/editing-2r
-          </a>{" "}
-          (Number of dimensions for RLACE)
-        </li>
-        <li>
-          <a href="https://www.kaggle.com/code/fabienroger/editing-3-8dim">
-            https://www.kaggle.com/code/fabienroger/editing-3-8dim
-          </a>{" "}
-          (Most effective layer for INLP)
-        </li>
-        <li>
-          <a href="https://www.kaggle.com/code/fabienroger/editing-3r-8dim">
-            https://www.kaggle.com/code/fabienroger/editing-3r-8dim
-          </a>{" "}
-          (Most effective layer for RLACE)
+        <h2>Editing Method</h2>
+        <ul>
           <li>
-            <a href="https://www.kaggle.com/code/fabienroger/editing-2-3-4-random">
-              https://www.kaggle.com/code/fabienroger/editing-2-3-4-random
-            </a>{" "}
-            (Measurements when choosing directions at random)
+            We measured model activations on the stereotype dataset and edited
+            the model to reduce this bias by applying INLP or RLACE on
+            intermediate activations of the neural network.
           </li>
-        </li>
-      </ul>
-    </Container>
+          <li>
+            Finding the relevant directions in the activations is a noisy
+            process. We repeat the process 5 times and show the result of each
+            run.
+          </li>
+          <li>
+            We then measured the relative probability between completions
+            following inputs with a female subject and inputs with male subjects
+            on both stereotypes data and the double bind data as validation.
+          </li>
+        </ul>
+        <Card className="section">
+          <CardHeader
+            className="section-title"
+            title="Observation 1: Bias is easier to remove in the middle of the network"
+          />
+          <CardContent className="section-content">
+            <p>
+              Applying RLACE or INLP to intermediate activations slightly
+              reduces the bias of the model. But it works better and generalize
+              a bit more to the validation set (the double bind sociology
+              experiment) when editing is done in the middle of the network,
+              where the network probably has a crisper representation of "male"
+              and "female".
+            </p>
+            <Grid container>
+              <Grid item xs={12} md={6}>
+                <Image name="inlp_8d_layer" />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <Image name="rlace_8d_layer" />
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Card>
+        <Card className="section">
+          <CardHeader
+            className="section-title"
+            title="Observation 2: The bias is not encoded in a linear way"
+          />
+          <CardContent className="section-content">
+            <p>
+              Though RLACE is able to make any linear classifier fail separation
+              between male and female with a rank-8 projection, we found that,
+              no matter where the projection is done, and how many dimensions
+              are removed, model bias isn't eliminated.
+            </p>
+            <Grid container>
+              <Grid item xs={12} md={6}>
+                <Image name="rlace_8d_layer" />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <Image name="rlace_dims" />
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Card>
+
+        <h2>Conclusion and upcoming improvements</h2>
+        <p>
+          Model exhibit easily <b>measurable bias</b> on small datasets from the
+          literature, augmented using CounterGen.{" "}
+          <b>
+            Direct model editing reduces the bias to a considerable extent while
+            requiring very few datapoints
+          </b>
+          . Still, it doesn't eliminate all the biases these model have, even
+          according to our narrow definition of bias.
+        </p>
+        <p>Some further experiments are needed:</p>
+        <ul>
+          <li>
+            Current experiments don't measure how much the edited models suffer
+            from the edition. Further experiment would{" "}
+            <b>measure the drop in performance due to edition</b> and compare it
+            to finetuning on the augmented dataset, and other bias reduction
+            methods.
+          </li>
+          <li>
+            The current edition method projects the data in a subspace of the
+            activation space. This requires a high degree of independence
+            between directions, which is not exactly the case: Anthropic showed
+            in their{" "}
+            <a
+              href="https://transformer-circuits.pub/2022/toy_model/index.html"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Polysemanticy write-up
+            </a>
+            that{" "}
+            <b>
+              rather than orthogonaility between vectors representing unrelated
+              concepts, we should expect "almost orthogonality"
+            </b>
+            . This suggests that other kinds of projection - such as projection
+            on a cone - might be able to remove relevant information about the
+            target concepts while keeping unrelated concepts intact.
+          </li>
+        </ul>
+        <p>If you are interested, please reach out!</p>
+        <h2>Additional remarks</h2>
+        <h3>Comparison with picking random dimensions</h3>
+        <p>
+          Projecting activation on random directions increases noise, and
+          reduces bias a little when many dimensions are removed. It performs
+          worse than applying INLP and RLACE to intermediate activations.
+        </p>
+        <Grid container>
+          <Grid item xs={12} md={6}>
+            <Image name="rdm_dims" />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Image name="rdm_8d_layer" />
+          </Grid>
+        </Grid>
+        <h3>Why are performance before edit different?</h3>
+        <p>
+          For each experiment, we pick the variations once. Because no new
+          variation is generated when editing the model, this is a fair and
+          slightly less noisy reference point we can compare the editing methods
+          against.
+        </p>
+        <h3>Where can I find the detail of the experiments?</h3>
+        <p>Here are the notebooks used to produce the results above:</p>
+        <ul>
+          <li>
+            <a href="https://github.com/FabienRoger/Countergen/blob/main/countergen/exploration/gpt_experiment.ipynbr">
+              https://github.com/FabienRoger/Countergen/blob/main/countergen/exploration/gpt_experiment.ipynbr
+            </a>{" "}
+            (Experiment on OpenAI's models)
+          </li>
+          <li>
+            <a href="https://www.kaggle.com/code/fabienroger/editing-2r">
+              https://www.kaggle.com/code/fabienroger/editing-2r
+            </a>{" "}
+            (Number of dimensions for RLACE)
+          </li>
+          <li>
+            <a href="https://www.kaggle.com/code/fabienroger/editing-3-8dim">
+              https://www.kaggle.com/code/fabienroger/editing-3-8dim
+            </a>{" "}
+            (Most effective layer for INLP)
+          </li>
+          <li>
+            <a href="https://www.kaggle.com/code/fabienroger/editing-3r-8dim">
+              https://www.kaggle.com/code/fabienroger/editing-3r-8dim
+            </a>{" "}
+            (Most effective layer for RLACE)
+            <li>
+              <a href="https://www.kaggle.com/code/fabienroger/editing-2-3-4-random">
+                https://www.kaggle.com/code/fabienroger/editing-2-3-4-random
+              </a>{" "}
+              (Measurements when choosing directions at random)
+            </li>
+          </li>
+        </ul>
+      </Container>
+      <Footer />
+    </>
   );
 };
 
